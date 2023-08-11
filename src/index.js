bf689cddc9056c756a6f532d23255da3576833cc
tranduy710 <tranduy7102002@gmail.com>
2023-08-11 16:19:49 +0700

    routes & controllers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
     express.urlencoded({
@@ -26,24 +28,8 @@ app.engine(
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views/"));
-app.get("layouts/main", (req, res) => res.render("main"));
-app.get("/", (req, res) => res.render("home"));
-
-app.get("/news", (req, res) => {
-    console.log(req.query.q);
-    res.render("news");
-});
-
-app.get("/search", (req, res) => {
-    res.render("search");
-});
-
-app.post("/search", (req, res) => {
-    console.log(req.body);
-    res.send("");
-});
-
-console.log(path);
+//Routes init
+route(app);
 
 //127.0.0.1 - localhost
 app.listen(port, () => {
